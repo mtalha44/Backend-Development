c2cc1cd93bf643098e840c5b46574881202d26bb
mtalha44 <mt4458649@gmail.com>
2025-01-13 23:00:45 +0500

    Starting Node js

---
+++ Nodejs/Day1/index.js
@@ -0,0 +1,9 @@
+const talha = require("./index2")
+
+console.log("Hello" , talha)
+
+
+
+//node js ? 
+//export & require
+//module wrapper function
---
+++ Nodejs/Day1/index2.js
@@ -0,0 +1,20 @@
+// node automatically wrapp it 
+// (function( exports , require , module ,  _filename , _dirname){
+
+//     const bilal = {
+//         name : "Hamza",
+//     age : "16",
+//     developer : false
+// }
+
+// module.exports = bilal;
+// })
+
+
+const bilal = {
+    name : "Hamza",
+    age : "16",
+    developer : false
+}
+
+module.exports = bilal;
